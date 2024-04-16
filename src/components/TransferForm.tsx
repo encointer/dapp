eb210658f89cf362c76beacdf19c25a7ce8a7691
Alain Brenzikofer <alain@integritee.network>
2024-04-16 11:38:04 +0200

    automatically set recipient address to self whenever connected wallet changes

--- src/components/TransferForm.tsx
+++ src/components/TransferForm.tsx
@@ -1,6 +1,6 @@
 import { useForm } from '@mantine/form';
 import { isValidWalletAddress } from '../utils';
-import { FC } from 'react';
+import {FC, useEffect} from 'react';
 import { Button, Stack, TextInput } from '@mantine/core';
 import { TNodeWithRelayChains } from '@paraspell/sdk';
 import {useWallet} from "../providers/WalletProvider.tsx";
@@ -21,13 +21,18 @@ type Props = {
 
 const TransferForm: FC<Props> = ({ onSubmit, loading }) => {
   const { selectedAccount } = useWallet();
+
+  useEffect(() => {
+    if (selectedAccount) form.values.address = selectedAccount.address;
+  }, [selectedAccount]);
+
   const form = useForm<FormValues>({
     initialValues: {
       from: 'Encointer',
       to: 'Kusama',
       currency: 'KSM',
       amount: 0.2,
-      address: selectedAccount? selectedAccount.address : '',
+      address: '',
       useApi: false,
     },
 
@@ -39,7 +44,7 @@ const TransferForm: FC<Props> = ({ onSubmit, loading }) => {
   return (
     <form onSubmit={form.onSubmit(onSubmit)}>
       <Stack>
-        <TextInput label="Amount KSM to send" placeholder="0.1" required {...form.getInputProps('amount')} />
+        <TextInput label="Amount KSM to send to same account on relaychain" placeholder="0.1" required {...form.getInputProps('amount')} />
         <Button type="submit" loading={loading}>
           Submit transaction
         </Button>
